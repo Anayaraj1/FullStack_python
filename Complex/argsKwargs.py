e9c469efe5feab7ae84f090f3b24589174fea402
Anayaraj1 <109724574+Anayaraj1@users.noreply.github.com>
2023-04-05 15:21:58 +0530

    args

--- Complex/argsKwargs.py
+++ Complex/argsKwargs.py
@@ -1,8 +1,45 @@
-mytup=(1,2,3,4,5,6,7,8,9,10)
+'''
+def funargs(n,*args):
+    print(f'i have{n}best friends their name are :\n')
 
-def functionalArguments(*args):
     for i in args:
         print(i)
 
-print("hellosssss")
-    
+
+n=6
+funargs(n,"Anaya","Farhan","Puja","kriti","jyoti","Aayushi")'''
+
+# mytup=(1,2,3,4,5,6,7,8,9,10)
+
+# def functionArgument(*args):
+#     for i in args:
+#         print(i)
+#     print(args)
+#     print(type(args))
+
+# functionArgument(mytup)
+
+# mylist=[1,2,3,4,5,6,7,8,9,10]
+
+# def functionArgument(name,*args):
+#     for i in args:
+#         print(i)
+#         print(type(args))
+#         print(type(name))
+#         print(name)
+
+# functionArgument(mylist,"ark")
+
+
+# myset={1,2,3,4,5,6,7,8,9,10}
+# def functionAegument(name,*args):
+#     for i in args:
+#         print(i)
+#         print(type(args))
+#         print(type(name))
+#         print(name)
+
+# functionAegument("ark",myset)
+
+
+#keyword args
