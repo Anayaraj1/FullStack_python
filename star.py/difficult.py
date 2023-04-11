9c947cdc9ae5df828d06d92330c444aa1ccc64bf
Anayaraj1 <109724574+Anayaraj1@users.noreply.github.com>
2023-04-11 23:34:47 +0530

    star

---
+++ star.py/difficult.py
@@ -0,0 +1,35 @@
+# for i in range(7):
+#     for j in range(6):
+#         if (i==4 or j==0) and (i==4,5):
+#             print("@",end=" ")
+#     print()
+
+# for i in range(7):
+#     for j in range(6):
+#         if (i==0 and j==4) or (i==1 and j==4,5) or(i==2 and j==4) or (i==3,4 and j==1,2,3,4,5) and (i==5 and j==4) or (i==6 or j==1,2):
+#             print("^",end="")
+#         else:
+#             pass
+#     print()
+
+
+
+# for i in range(7):
+#     for j in range(6):
+#         if (i==0 or i== 2 and j==4 ) or (i==1 and j==4 or j==5):
+#             print("*",end=" ")
+#         else:
+#             print(end=" ")
+#     print()
+
+    # * * * * * * 
+    #    * * 
+    #    * * 
+    #     * 
+    #     * 
+    #     * 
+     #    *
+
+# row=int(input("Enter the no of row"))
+# col=int(input("Enter the no of col "))
+
